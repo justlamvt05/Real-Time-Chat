e7465061d22bbcb28bee03812ddb03a15087f4a7
lamthon <lam20052802@gmail.com>
2026-04-24 16:10:09 +0700

    update: separate logic impl

--- src/main/java/com/lamthoncoding/realtimechat/service/KafkaConsumerService.java
+++ src/main/java/com/lamthoncoding/realtimechat/service/KafkaConsumerService.java
@@ -1,25 +1,9 @@
 package com.lamthoncoding.realtimechat.service;
 
 import com.lamthoncoding.realtimechat.dto.MessageKafkaDto;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.stereotype.Service;
 
-@Service
-@RequiredArgsConstructor
-@Slf4j
-public class KafkaConsumerService {
 
-    private final SimpMessagingTemplate messagingTemplate;
+public interface KafkaConsumerService {
+    void consume(MessageKafkaDto dto);
 
-    @KafkaListener(topics = "chat-message-topic", groupId = "realtime-chat-group")
-    public void consume(MessageKafkaDto dto) {
-        log.info("consume message: {}", dto.getContent());
-        messagingTemplate.convertAndSend(
-                "/topic/room/" + dto.getChatRoomId(),
-                dto
-        );
-    }
 }
--- src/main/java/com/lamthoncoding/realtimechat/service/KafkaProducerService.java
+++ src/main/java/com/lamthoncoding/realtimechat/service/KafkaProducerService.java
@@ -1,32 +1,9 @@
 package com.lamthoncoding.realtimechat.service;
 
-import com.lamthoncoding.realtimechat.dto.MessageKafkaDto;
 import com.lamthoncoding.realtimechat.entity.Message;
-import lombok.RequiredArgsConstructor;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-public class KafkaProducerService {
-
-    private final KafkaTemplate<String, MessageKafkaDto> kafkaTemplate;
-
-    public void sendMessage(Message message) {
-        MessageKafkaDto dto = new MessageKafkaDto();
-
-        dto.setId(message.getId());
-        dto.setChatRoomId(message.getChatRoom().getId());
-        dto.setSenderId(message.getSender().getId());
-        dto.setUsername(message.getSender().getUsername());
-        dto.setContent(message.getContent());
-        dto.setType(message.getType());
-        dto.setFileUrl(message.getFileUrl());
-        dto.setDeleted(message.getDeleted());
-        dto.setEdited(message.getEdited());
-
-        kafkaTemplate.send("chat-message-topic", dto);
 
 
-    }
+public interface KafkaProducerService {
+    void sendMessage(Message message);
+
 }
